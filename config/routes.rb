035cb07181329cc073cab8096dc6ba1ba252577e
github username <github email address>
2019-07-19 16:49:37 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    # This works
+    # get 'students', to: 'static#index'
+
+
+    # This works too.
+    resources :students, only: [:index]
 end
